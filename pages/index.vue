7c31bbb579f7b3d89eb41caf58ef661a31c7742f
Stefan Isarie <stefan.isarie@gmail.com>
2025-04-29 02:48:36 +0300

    Minor comments for the code and change of date display

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
   // Format the date in a human readable format
   const useFormatDate = (timestamp) => {
     const date = new Date(timestamp)
-    return new Intl.DateTimeFormat('en-US', {
+    return new Intl.DateTimeFormat('en-UK', {
       dateStyle: 'medium',
       timeStyle: 'short',
     }).format(date)
@@ -41,35 +41,41 @@
 <template>
   <div class="dark:bg-black dark:text-white">
     <div class="container max-w-7xl mx-auto py-6 px-3 sm:px-5 md:px-8 xl:px-0">
+      <!-- Loading indicator -->
       <div v-if="!loading">
         Loading images from NASA...
       </div>
+
+      <!-- Image of the day successfully fetched -->
       <div class="aspect-w-16 aspect-h-9 flex flex-col space-y-6" v-else-if="apod">
         <div class="flex items-end justify-between w-full">
+          <!-- Theme toggle button -->
           <div>
-            <!-- Theme Toggle Button -->
             <button @click="toggleTheme" class="self-end px-4 py-2 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 rounded-md shadow-md">
               Toggle {{ isDarkMode ? 'Light' : 'Dark' }} Mode
             </button>
           </div>
+          <!-- Image resolution toggle button -->
           <div>
             <button @click="handleImageLoad" class="self-end px-4 py-2 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 rounded-md shadow-md">Change to {{ lowResImage ? 'HD' : 'SD' }} resolution</button>
           </div>
         </div>
 
+        <!-- Image or video display -->
         <div>
           <img v-if="apod.media_type === 'image' && lowResImage" :src="apod.url" :alt="apod.title" class="object-cover w-full h-full rounded-2xl shadow-2xl dark:shadow-gray-700 shadow-gray-800" />
           <img v-if="apod.media_type === 'image' && !lowResImage" :src="apod.hdurl" :alt="apod.title" class="object-cover w-full h-full rounded-2xl shadow-2xl dark:shadow-gray-700 shadow-gray-800" />
           <iframe v-else-if="apod.media_type === 'video'" :src="apod.url" class="w-full h-full" frameborder="0" allowfullscreen ></iframe>
         </div>
 
+        <!-- Image details -->
         <h1 class="text-3xl font-bold">{{ apod.title }} @ {{ apod.date }} ({{ lowResImage ? 'Low' : 'High' }} resolution)</h1>
-
         <p class="italic">{{ apod.explanation }}</p>
         <p v-show="apod.copyright">&copy; {{ apod.copyright }}</p>
-
         <p class="text-gray-300 dark:text-gray-600 text-xs">Last fetched at: {{ useFormatDate(apod.timestamp) }}</p>
       </div>
+
+      <!-- Error grabing the image for NASA API -->
       <div class="text-red-500" v-else>
         <p class="font-bold">There is an issue getting the image of the day from NASA.</p>
         <p>{{ error }}</p>
