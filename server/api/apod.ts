3d1fadce8b9549c4d71254fa4e0d099522a83069
Stefan Isarie <stefan.isarie@gmail.com>
2025-04-29 03:08:25 +0300

    Destructure NASA API key from runtime config

--- server/api/apod.ts
+++ server/api/apod.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
+  const { NASA_API_KEY } = useRuntimeConfig()
   
-  const data = await $fetch(`https://api.nasa.gov/planetary/apod?api_key=${config.NASA_API_KEY}`)
+  const data = await $fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
 
   console.log('New picture of the day from NASA successfully fetched!')
   
